ce3d7251d96622fdb1cf928a27ed38921a5dcd47
Himanshu Chaudhary <himanshu.chaudhary@knoldus.com>
2020-09-17 17:04:47 +0530

    second commit

--- SecondJava.java
+++ SecondJava.java
@@ -1,6 +1,7 @@
 class SecondJava{
 	public static void main(String args[]){
 		System.out.println("Second java program to test jenkins");
-		System.out.println("webhook up testing in multi-branch");
+		System.out.println("webhook up testing in multi-branch second time");
+		
 	}
 }
