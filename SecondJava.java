5cfc1516dfe07775194b8bd4fad7f5376b4cdd0b
Himanshu Chaudhary <himanshu.chaudhary@knoldus.com>
2020-09-17 12:56:30 +0530

    webhook

--- SecondJava.java
+++ SecondJava.java
@@ -1,5 +1,6 @@
 class SecondJava{
 	public static void main(String args[]){
 		System.out.println("Second java program to test jenkins");
+		System.out.println("webhook up testing in multi-branch");
 	}
 }
